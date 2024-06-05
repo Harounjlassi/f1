b65981fe58315d2e7362bbd4d6a56bcec8c5aa7f
Harounjlassi <jlassi.haroun@esprit.tn>
2024-06-05 23:37:47 +0100

    center the picture

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Center(child: Text('heeloo'))));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg'),
+          ),
+        ),
+      ),
+    ),
+  );
 }
